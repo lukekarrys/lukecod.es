ad38593e11debad16e922e1131273abd92115128
Luke Karrys <lukekarrys@gmail.com>
2020-01-28 11:53:41 -0700

    remove remark autolink js and hide noscript message

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,13 @@
 exports.onRenderBody = ({ setBodyAttributes }) => {
   setBodyAttributes({ class: "theme-base-0d" })
 }
+
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  replaceHeadComponents(
+    getHeadComponents().filter(
+      ({ key }) =>
+        key !== "gatsby-remark-autolink-headers-script" &&
+        key !== "gatsby-remark-autolink-headers-style"
+    )
+  )
+}
